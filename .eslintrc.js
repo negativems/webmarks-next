38b7e29e704ce37a20ca1e2bdc883b658b185caa
mohamedbh <mohamedbenhamzaa@gmail.com>
2023-03-21 15:02:55 +0100

    ♻️ Adds eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+   env: {
+      browser: true,
+      es2021: true
+   },
+   extends: [
+      'next/core-web-vitals',
+      'standard-with-typescript'
+   ],
+   parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.json'
+   },
+   plugins: [
+      'react'
+   ],
+   rules: {
+      indent: ['error', 3],
+      semi: ['error', 'always'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/semi': ['error', 'always'],
+      '@typescript-eslint/indent': ['error', 3],
+      '@typescript-eslint/space-before-function-paren': ['error', 'never']
+   }
+};
