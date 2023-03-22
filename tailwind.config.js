e796f685c684510ed5ee3856581b00353031283b
mohamedbh <mohamedbenhamzaa@gmail.com>
2023-03-22 15:04:03 +0100

    ♻️ extends new tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
       './src/**/*.{js,ts,jsx,tsx}'
    ],
    theme: {
+      screens: {
+         sm: '640px',
+         md: '768px',
+         lg: '1024px',
+         xl: '1280px'
+      },
       extend: {
          colors: {
             accent: '#3AE000',
@@ -18,9 +24,14 @@ module.exports = {
             'light-theme-text': '#000000'
          },
          boxShadow: {
-            'solid-sm': '1px 1px 0px rgba(0, 0, 0, 0.25)',
-            'solid-md': '3px 3px 0px rgba(0, 0, 0, 0.25)',
-            'solid-xl': '5px 5px 0px rgba(0, 0, 0, 0.25)'
+            'solid-sm': '1px 1px 0px rgba(0, 0, 0, 0.15)',
+            'solid-md': '3px 3px 0px rgba(0, 0, 0, 0.15)',
+            'solid-xl': '5px 5px 0px rgba(0, 0, 0, 0.15)'
+         },
+         dropShadow: {
+            'solid-sm': '1px 1px 0px rgba(0, 0, 0, 0.15)',
+            'solid-md': '3px 3px 0px rgba(0, 0, 0, 0.15)',
+            'solid-xl': '5px 5px 0px rgba(0, 0, 0, 0.15)'
          }
       }
    },
